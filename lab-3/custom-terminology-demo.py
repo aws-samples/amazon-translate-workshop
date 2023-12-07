7e33a9d3856fcf0076545862a8e9378f6363ca12
Ralph Gimash <gimralph@amazon.com>
2023-12-07 09:14:10 -0800

    Changes to script and python code

--- lab-3/custom-terminology-demo.py
+++ lab-3/custom-terminology-demo.py
@@ -5,12 +5,12 @@ translate = boto3.client('translate')
 
 with open('custom_terminology.csv', 'rb') as ct_file:
     translate.import_terminology(
-        Name='CustomTerminology_boto3',
+        Name='custom-terminology-demo',
         MergeStrategy='OVERWRITE',
         Description='Terminology for Demo through boto3',
         TerminologyData={
             'File': ct_file.read(),
-            'Format': 'CSV'
+            'Format': 'CSV',
             'Directionality': 'MULTI'
         }
     )
@@ -20,7 +20,7 @@ terminology_names = [tag["Name"] for tag in response["TerminologyPropertiesList"
 print(str(terminology_names))
 
 response = translate.get_terminology(
-    Name='CustomTerminology_boto3',
+    Name='custom-terminology-demo',
     TerminologyDataFormat='CSV'
 )
 print("Name:{}".format(response["TerminologyProperties"]["Name"]))
@@ -34,7 +34,7 @@ OUTPUT_LANG_CODE = 'en'
 
 result = translate.translate_text(
     Text=SOURCE_TEXT,
-    TerminologyNames=['CustomTerminology_boto3'],
+    TerminologyNames=['custom-terminology-demo'],
     SourceLanguageCode='auto',
     TargetLanguageCode=OUTPUT_LANG_CODE
 )
